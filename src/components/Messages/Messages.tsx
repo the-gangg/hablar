7bbbd2de78e310f7a1dd607d0dc0a1ad503bf762
Michielu Menning <mich.menning@gmail.com>
2018-03-24 17:35:50 -0500

    Add Messages Component

---
+++ src/components/Messages/Messages.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { View } from 'react-native';
+
+import styles from './styles';
+
+const Messages = (props: {}) => (
+  <View style={styles.container}>
+  </View>
+);
+ export default Messages;
