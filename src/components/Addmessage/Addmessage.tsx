a9d100109dd1be3abc3c066919316ec6bce13297
Michielu Menning <mich.menning@gmail.com>
2018-03-24 17:54:37 -0500

    Create AddMessage Component

---
+++ src/components/Addmessage/Addmessage.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+import styles from './styles';
+
+const AddMessage = (props: {}) => (
+  <View style={styles.container}>
+    <Text> In New Message</Text>
+  </View>
+);
+export default AddMessage;
