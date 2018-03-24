1ae4acd137eceda1c5e71c7021784dbd76699d1a
Victor Amupitan <amupitan@iastate.edu>
2018-03-24 14:53:35 -0500

    add py script to auto create components

---
+++ create_component.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python3
+
+import sys
+import os
+
+
+def get_name_from_stdib():
+    name = input('What is the name of the component?\n> ').strip()
+    if len(name) == 0:
+        sys.stderr.write('Must have name. Try again\n')
+        sys.exit(-1)
+
+    return name.title()
+
+
+name = ''
+if len(sys.argv) > 1:
+    name = sys.argv[1].title()
+else:
+    name = get_name_from_stdib()
+
+# check if compoenent exists
+comp_path = './src/components/' + name
+if os.path.isdir(comp_path):
+    sys.stderr.write(
+        'Component with that name already exists. Try another name\n')
+    sys.exit(-1)
+
+# create component directory
+os.makedirs(comp_path)
+
+# add index.ts
+index_content = """import {} from './{}';
+ 
+export default {};
+""".format(name, name, name)
+file = open(comp_path+'/index.ts', "w")
+file.write(index_content)
+file.close()
+
+# add component file
+comp_content = """import * as React from 'react';
+import {{ View }} from 'react-native';
+
+import styles from './styles';
+
+const {} = (props: {{}}) => (
+  <View style={{styles.container}}>
+  </View>
+);
+ export default {};
+""".format(name, name)
+file = open(comp_path + '/' + name + '.tsx', "w")
+file.write(comp_content)
+file.close()
+
+test_content = """import * as React from 'react';
+import {} from './';
+
+import * as renderer from 'react-test-renderer';
+
+it('renders without crashing', () => {{
+  const rendered = renderer.create(<{} />).toJSON();
+  expect(rendered).toBeTruthy();
+}});
+
+""".format(name, name)
+file = open(comp_path + '/' + name + '.test.tsx', "w")
+file.write(test_content)
+file.close()
+
+styles_content = """import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+        marginTop: 25,
+        padding: 10,
+    },
+});
+"""
+file = open(comp_path + '/styles.ts', "w")
+file.write(styles_content)
+file.close()
