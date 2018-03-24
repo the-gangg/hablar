5d01ba2ec9bf8dd3a07d61dc1511559a48640897
Michielu Menning <mich.menning@gmail.com>
2018-03-24 12:28:22 -0500

    Styles for main app

--- src/components/App/styles.ts
+++ src/components/App/styles.ts
@@ -2,9 +2,26 @@ import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
     container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+        marginTop: 25,
+        padding: 10,
     },
+    header: {
+        fontSize: 20,
+    },
+    nav: {
+        flexDirection: 'row',
+        justifyContent: 'space-around'
+    },
+    navItem: {
+        flex: 1,
+        alignItems: 'center',
+        padding: 10,
+    },
+    subNavItem: {
+        padding: 5,
+    },
+    topic: {
+        textAlign: 'center',
+        fontSize: 15,
+    }
 });
