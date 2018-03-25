70bb25f39ae4bd73d826647cf08c614ff879f2a4
wseymour <wseymour@iastate.edu>
2018-03-24 19:36:00 -0500

    fixed js file

--- src/lib/sudobackend.js
+++ src/lib/sudobackend.js
@@ -21,13 +21,12 @@ function add(objectAdding, path) {
     ref.push(objectAdding);
 }
 // }
-<<<<<<< HEAD
-<<<<<<< HEAD
 newMessage({
     username: "Walter",
     text: "bye",
     language: "spanish"
 }, "-K2ib4H77rj0LYewF7dP");
+
 function getObject(path) {
     database.ref(path).on("value", function (snapshot) {
         console.log(snapshot.val());
@@ -35,25 +34,18 @@ function getObject(path) {
         console.log("Error: " + error.code);
     });
 }
+
 getObject("conversations/-K2ib4H77rj0LYewF7dP/messages/-L8PLyG6zcEOloHWTB4u");
-=======
-//update {values: object}
-=======
+
 //update = {values: object}
->>>>>>> c723629a1da2f687625365123b9f2d2b09ac348e
 function update(update, path) {
     var ref = database.ref(path);
     ref.update(update);
 }
-<<<<<<< HEAD
-update({ dm: false }, "conversations/" + "-K2ib4H77rj0LYewF7dP");
->>>>>>> be1ede76e36c1075048958f8be765424769942df
-=======
+
 //path: path to element
 //key: the key of the element to remove 
 function remove(path, key) {
     var ref = database.ref(path);
     ref.child(key).remove();
 }
-remove("conversations/" + "-K2ib4H77rj0LYewF7dP" + "/messages" + "-L8PFsyOnqCpf2ztEhWw", "language");
->>>>>>> c723629a1da2f687625365123b9f2d2b09ac348e
