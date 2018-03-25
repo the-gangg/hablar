1b5cc2baf3aee4d2f3f0089c64a7c419cbead947
Michielu Menning <mich.menning@gmail.com>
2018-03-24 22:46:43 -0500

    Add chat to home

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -7,6 +7,7 @@ import Messages from '../Messages';
 import Settings from "../Settings";
 import AddMessage from '../AddMessage';
 import Header from '../Header';
+import Chat from '../Chat';
 
 import styles from './styles';
 
@@ -19,6 +20,8 @@ export default class Home extends React.Component<{}> {
           <Route exact path="/messages" component={Messages} />
           <Route path="/addMessage" component={AddMessage} />
           <Route path="/settings" component={Settings} />
+          <Route path="/chat/:chatName" component={Chat} />
+          {/* <Route path="/chat/:chatName" component={(props: {}) => (<Chat {...props} />)} /> */}
         </View>
         <Nav />
       </View>
